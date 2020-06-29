b0009f2be19660b873ff4522ad23cd717f3a385d
Laisam <b540915@gmail.com>
2020-06-29 16:57:41 +0800

    feat: add authority control

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,29 @@ const passport = require('passport')
 
 module.exports = (app, passport) => {
 
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    else {
+      return res.redirect('/signin')
+    }
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  const authticateAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      else {
+        res.redirect('/')
+      }
+    }
+  }
+
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
+
+  app.get('/admin', authticateAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authticateAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
