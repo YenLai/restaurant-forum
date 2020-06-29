531fdf8a388d7eb637ab7623771fbaf5fd30924d
Laisam <b540915@gmail.com>
2020-06-29 15:15:53 +0800

    feat: add sign up function & page

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const PORT = 3000
+const db = require('./models')
+const { urlencoded } = require('body-parser')
 
 const app = express()
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.listen(PORT, () => {
---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const db = require('../models')
+const bcrypt = require('bcryptjs')
+const User = db.User
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: (req, res) => {
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+    }).then(user => {
+      return res.redirect('/signin')
+    })
+  }
+}
+
+module.exports = userController
