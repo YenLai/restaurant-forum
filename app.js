73fdbea4c125630ce2ca632c78e59f5c362d747e
Laisam <b540915@gmail.com>
2020-06-30 14:02:11 +0800

    feat: modify port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const PORT = 3000
+const port = process.env.PORT || 3000
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`The server is running on http://localhost:${PORT}`)
 })
 
