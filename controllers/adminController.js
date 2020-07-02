5720bc8db858e9447152194c90d2d32711d8a7f4
Laisam <b540915@gmail.com>
2020-07-02 18:53:27 +0800

    feat: add error handle

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,9 +9,11 @@ const User = db.User
 const adminController = {
   //  restaurants
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants })
-    })
+    return Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants })
+      })
+      .catch(err => console.log(err))
   },
 
   createRestaurant: (req, res) => {
@@ -34,11 +36,11 @@ const adminController = {
           address,
           opening_hours,
           description,
-          image: file ? img.data.link : null,
+          image: img.data.link,
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
-        })
+        }).catch(err => console.log(err))
       })
     } else {
       return Restaurant.create({
@@ -51,20 +53,24 @@ const adminController = {
       }).then(() => {
         req.flash('success_messages', '餐廳新增成功。')
         return res.redirect('/admin/restaurants')
-      })
+      }).catch(err => console.log(err))
     }
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/detail', { restaurant })
-    })
+    return Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        return res.render('admin/detail', { restaurant })
+      })
+      .catch(err => console.log(err))
   },
 
   editRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/create', { restaurant })
-    })
+    return Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        return res.render('admin/create', { restaurant })
+      })
+      .catch(err => console.log(err))
   },
 
   putRestaurant: (req, res) => {
@@ -80,59 +86,76 @@ const adminController = {
       imgur.upload(file.path, (err, img) => {
         return Restaurant.findByPk(req.params.id)
           .then((restaurant) => {
-            restaurant.update({
+            return restaurant.update({
               name,
               tel,
               address,
               opening_hours,
               description,
               image: file ? img.data.link : restaurant.image
-            }).then((restaurant) => {
-              req.flash('success_messages', 'restaurant was successfully to update')
-              res.redirect('/admin/restaurants')
             })
+              .then(() => {
+                req.flash('success_messages', 'restaurant was successfully to update')
+                res.redirect(`/admin/restaurants/${req.params.id}`)
+              })
+              .catch(err => console.log(err))
           })
+          .catch(err => console.log(err))
       })
     } else {
       return Restaurant.findByPk(req.params.id)
         .then((restaurant) => {
-          restaurant.update({
+          return restaurant.update({
             name,
             tel,
             address,
             opening_hours,
             description,
             image: restaurant.image
-          }).then((restaurant) => {
-            req.flash('success_messages', 'restaurant was successfully to update')
-            res.redirect('/admin/restaurants')
           })
+            .then(() => {
+              req.flash('success_messages', 'restaurant was successfully to update')
+              res.redirect(`/admin/restaurants/${req.params.id}`)
+            })
+            .catch(err => console.log(err))
         })
+        .catch(err => console.log(err))
     }
   },
 
   deleteRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id).then((restaurant) => {
-      restaurant.destroy()
-        .then(() => res.redirect('/admin/restaurants'))
-    })
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant
+          .destroy()
+          .then(() => res.redirect('/admin/restaurants'))
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
   },
 
   // users
   getUsers: (req, res) => {
-    return User.findAll({ raw: true }).then(users => {
-      return res.render('admin/users', { users })
-    })
+    return User.findAll({ raw: true })
+      .then(users => {
+        return res.render('admin/users', { users })
+      })
+      .catch(err => console.log(err))
   },
   putUsers: (req, res) => {
-    return User.findByPk(req.params.id).then(user => {
-      return user.update({
-        isAdmin: !user.isAdmin
+    return User.findByPk(req.params.id)
+      .then(user => {
+        return user
+          .update({
+            isAdmin: !user.isAdmin
+          })
+          .catch(err => console.log(err))
       })
-    }).then(() => {
-      req.flash('success_messages', 'user role成功更新!')
-      res.redirect('/admin/users')
-    })
+      .then(() => {
+        req.flash('success_messages', 'user role成功更新!')
+        res.redirect('/admin/users')
+      })
+      .catch(err => console.log(err))
   },
 }
 
